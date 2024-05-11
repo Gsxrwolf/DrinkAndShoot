3b1eea1a2a7d5e788de88ffa74666001adbfaf0e
Salanem <mail@jan-schmitz.dev>
2024-05-11 15:29:26 +0200

    Chore: put player scripts in Player namespace

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,55 +1,58 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[DisallowMultipleComponent]
-[RequireComponent(typeof(Rigidbody))]
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-    public Vector2 MovementInput { get; set; } = Vector2.zero;
-    public Vector2 RotationInput { get; set; } = Vector2.zero;
-
-    [SerializeField]
-    private float m_movementSpeed = 5.0f;
-    [SerializeField]
-    private float m_rotationSpeed = 60.0f;
-
-    [SerializeField]
-    private Transform m_pitchTransform = null;
-
-    private Rigidbody m_rigidbody = null;
-    private Camera m_camera = null;
-
-    private void Awake()
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerMovement : MonoBehaviour
     {
-        m_camera = Camera.main;
-        m_rigidbody = GetComponent<Rigidbody>();
-    }
+        public Vector2 MovementInput { get; set; } = Vector2.zero;
+        public Vector2 RotationInput { get; set; } = Vector2.zero;
 
-    private void Update()
-    {
-        Vector3 velocity = Vector3.Scale(m_camera.transform.forward, new Vector3(1, 0, 1)) * MovementInput.y 
-            + Vector3.Scale(m_camera.transform.right, new Vector3(1, 0, 1)) * MovementInput.x;
-        velocity = velocity.normalized * m_movementSpeed;
-        m_rigidbody.velocity = velocity;
+        [SerializeField]
+        private float m_movementSpeed = 5.0f;
+        [SerializeField]
+        private float m_rotationSpeed = 60.0f;
 
-        Vector3 pitchRotation = m_pitchTransform.localEulerAngles;
-        pitchRotation.x += RotationInput.y * m_rotationSpeed * Time.deltaTime;
-        
-        m_pitchTransform.localEulerAngles = pitchRotation;
+        [SerializeField]
+        private Transform m_pitchTransform = null;
 
-        Vector3 yawRotation = transform.localEulerAngles;
-        yawRotation.y += RotationInput.x * m_rotationSpeed * Time.deltaTime;
+        private Rigidbody m_rigidbody = null;
+        private Camera m_camera = null;
 
-        transform.localEulerAngles = yawRotation;
-    }
+        private void Awake()
+        {
+            m_camera = Camera.main;
+            m_rigidbody = GetComponent<Rigidbody>();
+        }
 
-    public void OnMove(InputAction.CallbackContext _context)
-    {
-        MovementInput = _context.ReadValue<Vector2>();
-    }
+        private void Update()
+        {
+            Vector3 velocity = Vector3.Scale(m_camera.transform.forward, new Vector3(1, 0, 1)) * MovementInput.y
+                + Vector3.Scale(m_camera.transform.right, new Vector3(1, 0, 1)) * MovementInput.x;
+            velocity = velocity.normalized * m_movementSpeed;
+            m_rigidbody.velocity = velocity;
 
-    public void OnRotate(InputAction.CallbackContext _context)
-    {
-        RotationInput = _context.ReadValue<Vector2>();
+            Vector3 pitchRotation = m_pitchTransform.localEulerAngles;
+            pitchRotation.x += RotationInput.y * m_rotationSpeed * Time.deltaTime;
+
+            m_pitchTransform.localEulerAngles = pitchRotation;
+
+            Vector3 yawRotation = transform.localEulerAngles;
+            yawRotation.y += RotationInput.x * m_rotationSpeed * Time.deltaTime;
+
+            transform.localEulerAngles = yawRotation;
+        }
+
+        public void OnMove(InputAction.CallbackContext _context)
+        {
+            MovementInput = _context.ReadValue<Vector2>();
+        }
+
+        public void OnRotate(InputAction.CallbackContext _context)
+        {
+            RotationInput = _context.ReadValue<Vector2>();
+        }
     }
 }
