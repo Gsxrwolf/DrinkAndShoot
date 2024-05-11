73b65f59b100968eee295109c6686a49df61f59e
Salanem <mail@jan-schmitz.dev>
2024-05-11 18:08:27 +0200

    Feature: return weapon pickup into pool when picked up

--- Assets/Scripts/Collectable/WeaponPickup.cs
+++ Assets/Scripts/Collectable/WeaponPickup.cs
@@ -1,3 +1,4 @@
+using Managers;
 using Player;
 using UnityEngine;
 using Weapons;
@@ -7,25 +8,43 @@ namespace Collectable
     [DisallowMultipleComponent]
     public class WeaponPickup : MonoBehaviour, ICollectable
     {
+        public EWeaponTypes Type => m_type;
+
+        [SerializeField]
+        private EWeaponTypes m_type = EWeaponTypes.BASE;
         [SerializeField]
         private ABaseWeapon m_weapon = null;
         [SerializeField]
         private float m_rotationSpeed = 50.0f;
+        [SerializeField]
+        private Transform m_representation = null;
 
         private Vector3 m_rotationAxis = Vector3.zero;
+        private WeaponPickupPool m_originPool = null;
+
         private void Awake()
         {
             m_rotationAxis = Random.onUnitSphere;
         }
 
+        public void AssignPool(WeaponPickupPool _pool)
+        {
+            m_originPool = _pool;
+        }
+
         public void Collect(GameObject _collector)
         {
-            _collector.GetComponent<PlayerActions>()?.EquipWeapon(m_weapon);
+            PlayerActions actions = _collector.GetComponent<PlayerActions>();
+            if (actions != null)
+            {
+                actions.EquipWeapon(m_weapon);
+                m_originPool.ReturnToPool(this);
+            }
         }
 
         private void Update()
         {
-            transform.Rotate(m_rotationAxis, m_rotationSpeed * Time.deltaTime);
+            m_representation.Rotate(m_rotationAxis, m_rotationSpeed * Time.deltaTime);
         }
     }
 }
